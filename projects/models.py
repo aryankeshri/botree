f9d4979ef51f64f09f557380013a62b63b87ab22
unknown <aryankeshri89@gmail.com>
2016-08-27 22:00:37 +0530

    add file validation and requirement.txt

--- projects/models.py
+++ projects/models.py
@@ -1,19 +1,10 @@
+import os
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.utils.text import slugify
+from django.core.exceptions import ValidationError
 
 # Create your models here.
-class Project(models.Model):
-    name_of_project = models.CharField(verbose_name='Name', max_length=50, blank=False, unique=True)
-    slug = models.SlugField(unique=True, max_length=50)
-
-    def __str__(self):
-        return self.name_of_project
-
-    def get_absolute_url(self):
-        return reverse("project_detail", kwargs={"slug": self.slug})
-
-
 
 def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
@@ -26,13 +17,34 @@ def create_slug(instance, new_slug=None):
         return create_slug(instance, new_slug=new_slug)
     return slug
 
+def validation_file_extension(value):
+    ext = os.path.splitext(value.name)[1]
+    valid_extension = ['.pdf']
+    if not ext.lower() in valid_extension:
+        raise ValidationError(u'Unsupported file extension.')
+
+class Project(models.Model):
+    name_of_project = models.CharField(verbose_name='Name', max_length=50, blank=False, unique=True)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name_of_project
+
+    def get_absolute_url(self):
+        return reverse("project_detail", kwargs={"slug": self.slug})
 
 class Document(models.Model):
     name_of_document = models.CharField(verbose_name='Name', max_length=100, blank=False, unique=True)
-    file = models.FileField(upload_to='pdf')
+    file = models.FileField(upload_to='pdf', validators=[validation_file_extension])
     project = models.ManyToManyField(Project)
     created = models.DateTimeField('created', auto_now_add=True)
     modified = models.DateTimeField('modified', auto_now=True)
 
     def __str__(self):
         return self.name_of_document
+
+
+
+
+
+
